becc39b7df3f01dd4af92a3d25a11dacf934df9c
prajwalkrishna2@gmail.com <prajwalkrishna2@gmail.com>
2021-09-12 17:29:57 -0400

    demojize has been updated

--- Models/Text_cleaning.py
+++ Models/Text_cleaning.py
@@ -50,19 +50,6 @@ def demoji(text):
     return(emoji_pattern.sub(r'', text))
 
 
-def demoji(text):
-    emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        u"\U00002702-\U000027B0"
-        u"\U000024C2-\U0001F251"
-        u"\U00010000-\U0010ffff"
-                               "]+", flags=re.UNICODE)
-    return(emoji_pattern.sub(r'', text))
-
-
 def main():
     # csv file
     input_file = ''
