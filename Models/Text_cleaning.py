a3170e07a4c51c7dc42837db64fd82c86d44f935
prajwalkrishna2@gmail.com <prajwalkrishna2@gmail.com>
2021-09-12 14:52:48 -0400

    main() has been implemented, the developement has been staleld due to the knowledge block in the conversion of JSON to CSV, when the JSON_to_CSV is completed,cleanign will be excuted flwalessly, this code has been tested by manually converting JSON to CSV using online tools https://www.convertcsv.com/json-to-csv.html

--- Models/Text_cleaning.py
+++ Models/Text_cleaning.py
@@ -50,6 +50,19 @@ def demoji(text):
     return(emoji_pattern.sub(r'', text))
 
 
+def demoji(text):
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U00010000-\U0010ffff"
+                               "]+", flags=re.UNICODE)
+    return(emoji_pattern.sub(r'', text))
+
+
 def main():
     # csv file
     input_file = ''
@@ -76,7 +89,7 @@ def main():
     dataset_df['data/text'] = dataset_df['data/text'].apply( lambda x : demoji(x))
 
     # convert df to csv
-    dataset_df.to_csv('cleaned_मुर्खा.csv',index = False)
+    dataset_df.to_csv('',index = False)
 
 if __name__ == "__main__":
     main()
